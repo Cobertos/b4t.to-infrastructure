15133cf29105869ede6d3d286f02b87dbeb93965
cobertos <me+git@cobertos.com>
2020-04-20 03:33:52 -0400

    Fixed up icecast for better passwords

---
+++ icecast/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [ -n "$ICECAST_SOURCE_PASSWORD" ]; then
+    sed -i "s/<source-password>[^<]*<\/source-password>/<source-password>$ICECAST_SOURCE_PASSWORD<\/source-password>/g" /usr/share/icecast/icecast.xml
+fi
+if [ -n "$ICECAST_RELAY_PASSWORD" ]; then
+    sed -i "s/<relay-password>[^<]*<\/relay-password>/<relay-password>$ICECAST_RELAY_PASSWORD<\/relay-password>/g" /usr/share/icecast/icecast.xml
+fi
+if [ -n "$ICECAST_ADMIN_PASSWORD" ]; then
+    sed -i "s/<admin-password>[^<]*<\/admin-password>/<admin-password>$ICECAST_ADMIN_PASSWORD<\/admin-password>/g" /usr/share/icecast/icecast.xml
+fi
+if [ -n "$ICECAST_ADMIN_USERNAME" ]; then
+    sed -i "s/<admin-user>[^<]*<\/admin-user>/<admin-user>$ICECAST_ADMIN_USERNAME<\/admin-user>/g" /usr/share/icecast/icecast.xml
+fi
+
+exec "$@"

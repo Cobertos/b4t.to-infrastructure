8b830a00ad0c043bb520d4c8b440ca3963e71a0e
Cobertos / Peter <me+git@cobertos.com>
2020-08-03 03:32:36 -0400

    fix(duplicati): Stop with the warnings, for real this time

--- duplicati/run-before-backup.sh
+++ duplicati/run-before-backup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl batto-svc/backup --fail --silent -H "Authorization: Bearer $BATTO_SCRIPT_AUTH"
+curl batto-svc/backup --fail --silent --show-error -H "Authorization: Bearer $BATTO_SCRIPT_AUTH"
